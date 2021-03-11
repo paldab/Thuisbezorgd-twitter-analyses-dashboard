aa0ca721ad7b9c7ec6aed66ea861dcc72b44f57c
K. Mylothridis <kostas.mylothridis@hva.nl>
2021-03-11 19:56:24 +0100

    General insert method created for tweets from tweepy searches

--- flask/data.py
+++ flask/data.py
@@ -17,42 +17,53 @@ class TweetCollector():
         if self.twint_config.Pandas:
             return twint.storage.panda.Tweets_df
 
+    def insert_tweets(self, item, session):
+        if item.geo is None:
+            location = item.geo
+        else:
+            # coordinates are represented in a list as: [lat, long]
+            location = ' '.join(map(str, item.geo['coordinates']))
+
+        tweet = model.Tweet(
+            text=item.text, created_at=item.created_at,
+            user_id=item.user.id, user_geo_enabled=item.user.geo_enabled,
+            user_screenname=item.user.screen_name, user_location=location,
+            retweet_count=item.retweet_count,
+            user_verified=item.user.verified
+        )
+
+        hashtags = item.entities['hashtags']
+
+        if hashtags:
+            for tag in hashtags:
+                hashtag = model.Hashtag(name=tag['text'])
+                q = session.query(model.Hashtag.id).filter(
+                    model.Hashtag.name == tag['text']
+                )
+
+                # Set id of Hastag when existing is found
+                if session.query(q.exists()).scalar():
+                    hashtag.id = q.first()[0]
+
+                tweet.hashtags.append(hashtag)
+
+            session.merge(tweet)
+            session.commit()
+
+    def archive_search(self, session, env):
+        cursor = tweepy.Cursor(self.api.search_full_archive,
+                               environment_name=env,
+                               query='@Thuisbezorgd OR #thuisbezorgd')
+
+        for item in cursor.items():
+            self.insert_tweets(item, session)
+
     def recent_search(self, session):
         cursor = tweepy.Cursor(self.api.search,
                                q='@Thuisbezorgd OR #thuisbezorgd', lang='nl')
 
         for item in cursor.items():
-            if item.geo is None:
-                location = item.geo
-            else:
-                # coordinates are represented in a list as: [lat, long]
-                location = ' '.join(map(str, item.geo['coordinates']))
-
-            tweet = model.Tweet(
-                text=item.text, created_at=item.created_at,
-                user_id=item.user.id, user_geo_enabled=item.user.geo_enabled,
-                user_screenname=item.user.screen_name, user_location=location,
-                retweet_count=item.retweet_count,
-                user_verified=item.user.verified
-            )
-
-            hashtags = item.entities['hashtags']
-
-            if hashtags:
-                for tag in hashtags:
-                    hashtag = model.Hashtag(name=tag['text'])
-                    q = session.query(model.Hashtag.id).filter(
-                        model.Hashtag.name == tag['text']
-                    )
-
-                    # Set id of Hastag when existing is found
-                    if session.query(q.exists()).scalar():
-                        hashtag.id = q.first()[0]
-
-                    tweet.hashtags.append(hashtag)
-
-                session.merge(tweet)
-                session.commit()
+            self.insert_tweets(item, session)
 
 
 session = Session()
@@ -61,6 +72,7 @@ c = twint.Config()
 model.Base.metadata.create_all(bind=engine)
 collector = TweetCollector(config.twitter['key'], config.twitter['secret'])
 collector.recent_search(session)
+# collector.archive_search(session, config.twitter['environment'])
 
 c.Search = '#thuisbezorgd'
 c.Since = '2020-12-31'
