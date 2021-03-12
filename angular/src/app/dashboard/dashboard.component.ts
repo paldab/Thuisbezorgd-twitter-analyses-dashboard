97d6c6e8e043615cea94d045ee2be4415ee24f57
Noman Jabbar <noman.jabbar@hva.nl>
2021-03-12 12:48:58 +0100

    add plot samples

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
@@ -7,29 +7,117 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   @Input() title!: string | undefined;
 
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
+    map(({matches}) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          {
+            title: "A Fancy Plott",
+            cols: 1,
+            rows: 1,
+            data: [
+              { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+              { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+            ],
+            layout: {width: 600, height: 400}
+          },
+          {
+            title: "A Fancy Plot",
+            cols: 1,
+            rows: 1,
+            data: [
+              { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'blue'} },
+              { x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y: [2, 5, 3, 6, 4, 7, 8, 10, 9, 4], type: 'bar' },
+            ],
+            layout: {width: 600, height: 400}
+          }
+          // {
+          //   title: 'Card 2',
+          //   cols: 1,
+          //   rows: 1
+          // },
+          // {
+          //   title: 'Card 3',
+          //   cols: 1,
+          //   rows: 1
+          // },
+          // {
+          //   title: 'Card 4',
+          //   cols: 1,
+          //   rows: 1
+          // }
         ];
       }
 
+      // return [{
+      //     title: 'Card 1',
+      //     cols: 2,
+      //     rows: 1
+      //   },
+        // {
+        //   title: 'Card 2',
+        //   cols: 1,
+        //   rows: 1
+        // },
+        // {
+        //   title: 'Card 3',
+        //   cols: 1,
+        //   rows: 2
+        // },
+        // {
+        //   title: 'Card 4',
+        //   cols: 1,
+        //   rows: 1
+        // }
+      // ];
+
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        {
+          title: "A Fancy Plott",
+          cols: 1,
+          rows: 1,
+          data: [
+            { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+            { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+          ],
+          layout: {width: 600, height: 400}
+        },
+        {
+          title: "A Fancy Plot",
+          cols: 1,
+          rows: 1,
+          data: [
+            { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'blue'} },
+            { x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y: [2, 5, 3, 6, 4, 7, 8, 10, 9, 4], type: 'bar' },
+          ],
+          layout: {width: 600, height: 400}
+        }
+        // {
+        //   title: 'Card 2',
+        //   cols: 1,
+        //   rows: 1
+        // },
+        // {
+        //   title: 'Card 3',
+        //   cols: 1,
+        //   rows: 1
+        // },
+        // {
+        //   title: 'Card 4',
+        //   cols: 1,
+        //   rows: 1
+        // }
       ];
     })
-  );
-
+  );  
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    console.log(this.cards);
+    
+  }
 }
