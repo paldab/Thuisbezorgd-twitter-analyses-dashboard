424ce13bb22648dade2344162a70284909cae6f3
K. Mylothridis <kostas.mylothridis@hva.nl>
2021-04-07 13:01:08 +0200

    Small fix

--- flask/app.py
+++ flask/app.py
@@ -5,80 +5,84 @@ from sqlalchemy import text
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import textwrap
-import twint 
+import twint
 import config
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
 # basic GET route
 @app.route('/welcome/', methods=['GET'])
 def welcome():
-	return "Welcome to localhost:5050"
+    return "Welcome to localhost:5050"
+
 
 # GET route with param
 @app.route('/person/<name>', methods=['GET'])
 def test(name):
-	return 'Welcome ' + name
+    return 'Welcome ' + name
+
 
 # GET route with multiple params and JSON response with 418 status code
 @app.route('/api/v1/all-tweets', methods=['GET'])
 def all_tweets():
-	
-	filter = request.args.get('f', default=None, type=str)
+    filter = request.args.get('f', default=None, type=str)
 
-	if filter == 'd':
-		statement = text("SELECT id, text, user_screenname, created_at FROM tweet WHERE DATE(created_at)=CURDATE()").\
-		columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
-		
-	if filter == 'w':
-		statement = text("SELECT id, text, user_screenname, created_at FROM tweet WHERE WEEK (created_at) >= WEEK(CURDATE()) -1 AND YEAR(created_at) = YEAR(CURDATE())").\
-		columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
+    if filter == 'd':
+        statement = text("SELECT id, text, user_screenname, created_at FROM tweet WHERE DATE(created_at)=CURDATE()").\
+            columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
 
-	if filter == 'm':
-		statement = text("SELECT id, text, user_screenname, created_at FROM tweet WHERE created_at > NOW() - INTERVAL 1 MONTH;").\
-		columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
+    if filter == 'w':
+        statement = text("SELECT id, text, user_screenname, created_at FROM tweet WHERE WEEK (created_at) >= WEEK(CURDATE()) -1 AND YEAR(created_at) = YEAR(CURDATE())").\
+            columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
 
-	if filter == '*':
-		statement = text("SELECT id, text, user_screenname, created_at FROM tweet").\
-		columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
-	
-	tweets = getattr(db, '_session')().\
-		query(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at).\
-		from_statement(statement).\
-		all()
-		
-	row_headers=[x for x in tweets[0].keys()]
-	row_headers.append('trimmed_text')
-	json_data=[]
-	
-	for tweet in tweets:
-		trimmed_text = textwrap.shorten(tweet['text'], width=144, placeholder="...")
+    if filter == 'm':
+        statement = text("SELECT id, text, user_screenname, created_at FROM tweet WHERE created_at > NOW() - INTERVAL 1 MONTH;").\
+            columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
 
-		tweet = (tweet['id'], tweet['text'], tweet['user_screenname'], tweet['created_at'], trimmed_text)
+    if filter == '*':
+        statement = text("SELECT id, text, user_screenname, created_at FROM tweet").\
+            columns(Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at)
 
-		json_data.append(dict(zip(row_headers, tweet)))
+    tweets = getattr(db, '_session')().query(
+        Tweet.id, Tweet.text, Tweet.user_screenname, Tweet.created_at
+    ).from_statement(statement).all()
 
-	return jsonify(json_data), 200
-    
+    row_headers = [x for x in tweets[0].keys()]
+    row_headers.append('trimmed_text')
+    json_data = []
+
+    for tweet in tweets:
+        trimmed_text = textwrap.shorten(tweet['text'], width=144,
+                                        placeholder="...")
+
+        tweet = (tweet['id'], tweet['text'],
+                 tweet['user_screenname'], tweet['created_at'], trimmed_text)
+
+        json_data.append(dict(zip(row_headers, tweet)))
+
+    return jsonify(json_data), 200
+
+
 if __name__ == '__main__':
+    c = twint.Config()
+    c.Search = '#thuisbezorgd OR @Thuisbezorgd'
+    c.Since = '2020-12-31'
+    c.Lang = 'nl'
+    c.Store_object = True
+    c.Hide_output = False
+    # c.Limit = 1
 
-	c = twint.Config()
+    getattr(db, '_base').metadata.create_all(bind=getattr(db, '_engine'))
 
-	c.Search = '#thuisbezorgd OR @Thuisbezorgd'
-	c.Since = '2020-12-31'
-	c.Lang = 'nl'
-	c.Store_object = True
-	c.Hide_output = False
-	# c.Limit = 1
+    collector = TweetCollector(
+        config.twitter['key'], config.twitter['secret'], c
+    )
+    # collector.recent_search(getattr(db, 'session'))
 
-	getattr(db, '_base').metadata.create_all(bind=getattr(db, '_engine'))
+    # collector.archive_search(getattr(db, 'session'),
+    #                          config.twitter['environment'])
+    # collector.twint_search()
 
-	# print(getattr(db, '_inspector').get_columns('tweet'))
-
-	collector = TweetCollector(config.twitter['key'], config.twitter['secret'], c)
-	# collector.recent_search(session)
-	# collector.archive_search(session, config.twitter['environment'])
-	# collector.twint_search()
-
-	app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
