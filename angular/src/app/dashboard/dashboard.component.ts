28f9b594cc98e28e931ef3784e7d294afed87c8b
imadkb7 <imad.baktit@hva.nl>
2021-04-13 18:51:10 +0200

    table changes when graph changes

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -15,7 +15,7 @@ export class DashboardComponent implements OnInit {
   name: any = undefined;  
  
   constructor(private breakpointObserver: BreakpointObserver, private tweetsService: TweetsService) {
-    this.mostRecentTweets();
+    this.mostRecentTweets('x');
 
     setTimeout(() => {
       this.name = window.sessionStorage.getItem('user_name')?.replace(/['"]+/g, '');
@@ -34,26 +34,20 @@ export class DashboardComponent implements OnInit {
   }
 
   getAllTweetsMonth() {
+    this.mostRecentTweets('x');
+
     this.createDate.length = 0;
     this.tweetsADay.length = 0;
     this.tweetsService.all_tweets('m').subscribe(
       data => {
-        console.log(data);
-        console.log(data[0].created_at.substr(0, 17))
         let counter = 0;
         for (let index = 1; index < data.length -1; index++) {
           if (data[index -1].created_at.substr(5, 7) !== data[index].created_at.substr(5, 7) ) {
           counter = counter + 1;
-          console.log(data[index].created_at);
           this.createDate.push(data[index].created_at.substr(5,7));
           }
         }
-
-        console.log(this.createDate);
-
-
           let teller = 0;
-
         for (let index = 0; index < this.createDate.length; index++) {
           let tweetscounter = 0;
           for (let index = 0; index < data.length; index++) {
@@ -64,19 +58,12 @@ export class DashboardComponent implements OnInit {
           teller = teller + 1;
           this.tweetsADay[index] = tweetscounter;
         }
-
-          console.log(this.createDate);
-          console.log(this.tweetsADay);
-        
-
       },
       err => {
         this.req_succeeded = err.ok
         console.error(err);
       }
     );    
-
-
     setTimeout(() => {
       if (this.req_succeeded == false) {
       } else {
@@ -84,29 +71,19 @@ export class DashboardComponent implements OnInit {
     }, 7000)
   }
 
-
-
   getAllTweetsDay() {
+    this.mostRecentTweets('d');
     this.createDate.length = 0;
     this.tweetsADay.length = 0;
     this.tweetsService.all_tweets('d').subscribe(
       data => {
-        console.log(data);
-        console.log(data[0].created_at.substr(0, 17))
         let counter = 0;
-        for (let index = 1; index < data.length -1; index++) {
-          
+        for (let index = 1; index < data.length -1; index++) {   
           counter = counter + 1;
           console.log(data[index].created_at);
           this.createDate.push(data[index].created_at.substr(5,7));
-          
         }
-
-        console.log(this.createDate);
-
-
           let teller = 0;
-
         for (let index = 0; index < this.createDate.length; index++) {
           let tweetscounter = 0;
           for (let index = 0; index < data.length; index++) {
@@ -116,20 +93,12 @@ export class DashboardComponent implements OnInit {
           }
           teller = teller + 1;
           this.tweetsADay[index] = tweetscounter;
-        }
-
-          console.log(this.createDate);
-          console.log(this.tweetsADay);
-        
-
+        }      
       },
       err => {
         this.req_succeeded = err.ok
-        console.error(err);
       }
     );    
-
-
     setTimeout(() => {
       if (this.req_succeeded == false) {
       } else {
@@ -137,9 +106,8 @@ export class DashboardComponent implements OnInit {
     }, 7000)
   }
 
-
-
   getAllTweetsWeek() {
+    this.mostRecentTweets('w');
     this.createDate.length = 0;
     this.tweetsADay.length = 0;
     this.tweetsService.all_tweets('w').subscribe(
@@ -154,12 +122,7 @@ export class DashboardComponent implements OnInit {
           this.createDate.push(data[index].created_at.substr(5,7));
           }
         }
-
-        console.log(this.createDate);
-
-
-          let teller = 0;
-
+         let teller = 0;
         for (let index = 0; index < this.createDate.length; index++) {
           let tweetscounter = 0;
           for (let index = 0; index < data.length; index++) {
@@ -170,18 +133,12 @@ export class DashboardComponent implements OnInit {
           teller = teller + 1;
           this.tweetsADay[index] = tweetscounter;
         }
-
-          console.log(this.createDate);
-          console.log(this.tweetsADay);
-        
-
       },
       err => {
         this.req_succeeded = err.ok
         console.error(err);
       }
     );    
-
 
     setTimeout(() => {
       if (this.req_succeeded == false) {
@@ -190,11 +147,9 @@ export class DashboardComponent implements OnInit {
     }, 7000)
   }
 
-
   /** Based on the screen size, switch from standard to one column per row */
   timeline = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({matches}) => {
-    
       return [
         {
           title: "Timeline tweets",
@@ -205,41 +160,42 @@ export class DashboardComponent implements OnInit {
               x: this.createDate, 
               y: this.tweetsADay, 
               type: 'bar',
-              
               marker: {
                 color: '#ff9800'
-              }
-            },
+              }},
           ],
           layout: {width: 600, height: 400}
         }
-    
       ];
     })
   );  
 
+mostRecentTweets(char: string): any{
+//   this.tweetsService.all_tweets(char).subscribe(
+//     data => {
+// let teller = 0;
+//       for (let index = data.length; index > data.length - this.countable; index--) {
+//         this.orderedTweetsArray[teller] = data[index-1];
+//         teller = teller + 1;
+//         console.log(index); 
+//       }
+// });
 
 
-mostRecentTweets(): any{
-  this.tweetsService.all_tweets('x').subscribe(
-    data => {
+this.tweetsService.all_tweets(char).subscribe(
+  data => {
 let teller = 0;
-      for (let index = data.length; index > data.length - this.countable; index--) {
-        this.orderedTweetsArray[teller] = data[index-1];
-        teller = teller + 1;
-        console.log(index); 
-      }
-
-
-    
-
-
-
-
+    for (let index = 0; index < this.countable; index++) {
+      this.orderedTweetsArray[teller] = data[index];
+      teller = teller + 1;
+      console.log(index); 
+    }
 });
+
+
+
+
+
+
 }
-
-
-
-
 }
