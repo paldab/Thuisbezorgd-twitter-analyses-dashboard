f4a4e5af13b060720b0436963705eff339c129d3
K. Mylothridis <kostas.mylothridis@hva.nl>
2021-04-26 14:42:15 +0200

    Removed test code and imports + Wordcloud created for cluster

--- flask/cluster.py
+++ flask/cluster.py
@@ -1,115 +1,94 @@
 from models.model import Tweet
 from models.database import db
-from utils.cleaner import clean_tweet, remove_stopwords
+from utils.cleaner import clean_tweet, remove_stopwords, stem_tweets
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans, AgglomerativeClustering
-from sklearn.decomposition import TruncatedSVD
-from sklearn.pipeline import make_pipeline
 from sklearn.metrics import silhouette_score
+from scipy.cluster.hierarchy import dendrogram
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
-import sys
-sys.setrecursionlimit(10000)
+from wordcloud import WordCloud
 
-tweets = db.session.query(Tweet.text).all()
+tweets = db.session.query(Tweet.text, Tweet.user_screenname).all()
 
-tweet_df = pd.DataFrame(tweets, columns=['text'])
+# Create DF and clean the tweets
+tweet_df = pd.DataFrame(tweets, columns=['text', 'user'])
 tweet_df = clean_tweet(tweet_df)
 tweet_df = remove_stopwords(tweet_df)
+tweet_df = stem_tweets(tweet_df)
 
-vectorizer = HashingVectorizer()
+vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(tweet_df.text)
 
-# features = vectorizer.get_feature_names()
+features = vectorizer.get_feature_names()
 
-# import functools
-# import numpy as np
 
-# nzero_count = 0
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
 
-# # tmp = np.where(np.all(np.isclose(X, 0.), axis=0))
-# print(X.shape)
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
 
-# for vector in X:
-#     print(vector)
-#     nonzero = np.count_nonzero(vector.toarray())
-#     nzero_count += nonzero
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
 
-    # tmp = np.where(vector == 0)
-    # print(tmp, type(tmp))
-    # if tmp == 0:
-    #     print(zero_count)
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
 
-#     zero_count += 1
 
-Z = linkage(X.todense(), 'average')
+model = AgglomerativeClustering(n_clusters=None, affinity='euclidean',
+                                distance_threshold=0, linkage='ward')
 
-cluster = fcluster(Z, 0.85, criterion='distance')
+model = model.fit(X.todense())
 
+plt.title('Hierarchical Clustering Dendrogram')
+
+# Plot the top three levels of the dendrogram
 plt.figure(figsize=(24, 9))
-dn = dendrogram(Z, show_leaf_counts=True)
-plt.savefig('test.svg')
+plot_dendrogram(model, show_leaf_counts=True)
+plt.xlabel('Number of points in node (or index of point if no parenthesis).')
+plt.savefig('agglomerative.svg')
 
-# model = AgglomerativeClustering(n_clusters=None, affinity='cosine',
-#                                 distance_threshold=0, linkage='single')
+clusters = 4
+top = 20
 
-# cluster = model.fit(X.toarray())
+kmeans = KMeans(n_clusters=clusters, init='k-means++',
+                n_init=1, random_state=1)
+kmeans.fit(X)
+labels = kmeans.predict(X)
 
-# # Create a TruncatedSVD instance: svd
-# svd = TruncatedSVD(n_components=50)
+print('Top %d terms per cluster:' % top)
 
-# # Create a KMeans instance: kmeans
-# kmeans = KMeans(n_clusters=6, random_state=0)
+# Get centroid coords
+order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]
 
-# # Create a pipeline: pipeline
-# pipeline = make_pipeline(svd, kmeans)
+print(silhouette_score(X, labels=labels))
 
-# pipeline.fit(X)
+for i in range(clusters):
+    print('Cluster %d:' % i)
 
-# labels = pipeline.predict(X)
-# model = KMeans(n_clusters=6, init='k-means++', random_state=0)
-# model.fit(X)
-# labels = model.predict(X)
-# print(labels)
+    cluster_list = []
 
-# ks = range(1, 20)
-# inertias = []
+    for ind in order_centroids[i]:
+        cluster_list.append(features[ind])
+        # print(' %s' % features[ind])
 
-# for k in ks:
-#     model = KMeans(n_clusters=k)
-#     model.fit(X)
+    # Create wordcloud for each cluster
+    joined_list = ' '.join(cluster_list)
+    wordcloud = WordCloud(width=1000, height=1000,
+                          background_color='white').generate(joined_list)
 
-#     inertias.append(model.inertia_)
-
-# plt.plot(ks, inertias, '-o')
-# plt.xlabel('number of clusters, k')
-# plt.ylabel('inertia')
-# plt.xticks(ks)
-# plt.show()
-
-# true_k = 5
-# top = 10
-
-# model = KMeans(n_clusters=true_k, init='k-means++', max_iter=200,
-#                n_init=1, random_state=1)
-# model.fit(X)
-# labels = model.predict(X)
-
-# print("Top %d terms per cluster:" % top)
-
-# order_centroids = model.cluster_centers_.argsort()[:, ::-1]
-
-# print(silhouette_score(X, labels=labels))
-
-# for i in range(true_k):
-#     print("Cluster %d:" % i)
-
-#     for ind in order_centroids[i, :top]:
-#         print(' %s' % features[ind])
-#     print()
-
-# model_random = KMeans(n_clusters=100, init='random', max_iter=100)
-# model_random.fit(X)
-# Hierarchical cluster
-# Dendogram
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.savefig('wordcloud-{}.svg'.format(i))
+    print()
