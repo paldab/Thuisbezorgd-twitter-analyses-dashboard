8bd51f8b20b3c0ac18c27076581f353dea2a32e7
Noman Jabbar <noman.jabbar@hva.nl>
2021-05-06 13:18:31 +0200

    increase timeout duration, wat as long as a watching paint dry and extract saving to a method.

--- flask/utils/scraper.py
+++ flask/utils/scraper.py
@@ -7,6 +7,14 @@ from pathlib import Path
 import pandas as pd
 from progressbar import printProgressBar
 
+def backup():
+    hotel_reviews_df = pd.DataFrame(data=scraped_reviews, columns=['review', 'rating'])
+
+    csv_path = (Path(__file__).parent.parent / "datasets/reviews.csv").resolve()
+
+    hotel_reviews_df.to_csv(csv_path, index=False)
+
+    print("\nCreated csv file in datasets folder.")
 
 if __name__ == '__main__':
     scraped_reviews = []
@@ -30,47 +38,45 @@ if __name__ == '__main__':
 
     button = browser.find_elements_by_css_selector("nav.pagination-container a.button.next-page")
     i = 0
-    while len(button) != 0:
-        time.sleep(1)
-        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.review-list")))
-        total_reviews = browser.find_element_by_class_name("headline__review-count").text.replace(".", "")
+    try:
+        while len(button) != 0:
+            time.sleep(2)
+            WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.review-list")))
+            total_reviews = browser.find_element_by_class_name("headline__review-count").text.replace(".", "")
 
-        review_list = browser.find_elements_by_css_selector("div.review-list div.review-card article.review")
+            review_list = browser.find_elements_by_css_selector("div.review-list div.review-card article.review")
 
-        for review in review_list:
+            for review in review_list:
+                time.sleep(0.4)
+                rev_content = review.find_elements_by_css_selector("section div.review-content div.review-content__body p.review-content__text")
+                rev_rating = review.find_elements_by_css_selector("section div.review-content div.review-content__header div.star-rating img")[0].get_attribute('alt')
+                # check if review has a description, if not move on.
+                if len(rev_content) == 0:
+                    continue;
+                
+                rev_content = rev_content[0].text.replace('\n', '')
+                rev_rating = rev_rating.split(' ')[0]
+
+                scraped_reviews.append([
+                    rev_content, rev_rating
+                ])
+
             time.sleep(0.4)
-            rev_content = review.find_elements_by_css_selector("section div.review-content div.review-content__body p.review-content__text")
-            rev_rating = review.find_elements_by_css_selector("section div.review-content div.review-content__header div.star-rating img")[0].get_attribute('alt')
-            # check if review has a description, if not move on.
-            if len(rev_content) == 0:
-                continue;
-            
-            rev_content = rev_content[0].text.replace('\n', '')
-            rev_rating = rev_rating.split(' ')[0]
+            button = browser.find_elements_by_css_selector("nav.pagination-container a.button.next-page")
 
-            scraped_reviews.append([
-                rev_content, rev_rating
-            ])
+            if len(button) != 0:
+                browser.execute_script("arguments[0].scrollIntoView({block: \"end\", inline: \"nearest\"});", button[0])
+                button[0].click()
 
-        time.sleep(0.4)
-        button = browser.find_elements_by_css_selector("nav.pagination-container a.button.next-page")
+            i += 20
+            printProgressBar(i, int(total_reviews), prefix = 'Progress:', suffix = 'Complete', length = 50)
+        
+        browser.quit()
+        backup()
 
-        if len(button) != 0:
-            browser.execute_script("arguments[0].scrollIntoView({block: \"end\", inline: \"nearest\"});", button[0])
-            button[0].click()
-
-        i += 20;
-        printProgressBar(i, int(total_reviews), prefix = 'Progress:', suffix = 'Complete', length = 50)
-
-
+    except Exception as e:
+        backup()
+        pass
+    
     # filter emojis from dataframe
     # df.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
-    browser.quit()
-
-    hotel_reviews_df = pd.DataFrame(data=scraped_reviews, columns=['review', 'rating'])
-
-    csv_path = (Path(__file__).parent.parent / "datasets/reviews.csv").resolve()
-
-    hotel_reviews_df.to_csv(csv_path, index=False)
-
-    print("\nCreated csv file in datasets folder.")
